3ba5bb82382e19653e0f982da7f3cfde6f08281c
Darkarotte <The127@users.noreply.github.com>
2025-08-25 15:21:52 +0200

    Add Debug.vue for testing login flow and register `/debug` route in router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
     {
+        path: '/debug',
+        name: 'debug',
+        component:  () => import('../views/Debug.vue'),
+    },
+    {
         path: '/login',
         name: 'login',
         component:  () => import('../views/Login.vue'),
---
+++ src/views/Debug.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import Button from "../components/Button.vue";
+
+const startLoginFlow = () => {
+  window.location.href = 'http://127.0.0.1:8081/oidc/keyline/authorize?client_id=admin-ui&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code&scope=oidc+email+profile'
+}
+
+</script>
+
+<template>
+  <Button text="Start Login Flow" @click="startLoginFlow"/>
+</template>
+
+<style scoped>
+
+</style>
