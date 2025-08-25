cdda3827a54af0f93a47d43e4b687372e51c03e4
Darkarotte <The127@users.noreply.github.com>
2025-08-25 14:12:35 +0200

    Integrate `vue-query` with Login.vue for token-based data fetching and set up `VueQueryPlugin` in main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import {i18n} from "./i18n.js";
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const app = createApp(App);
 app.use(router)
 app.use(i18n)
+app.use(VueQueryPlugin)
 app.mount('#app')
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,6 +4,7 @@ import {useI18n} from "vue-i18n";
 import Form from "../components/Form.vue";
 import Input from "../components/Input.vue";
 import {useRoute} from "vue-router";
+import {useQuery} from "@tanstack/vue-query";
 
 const { t } = useI18n({
   messages: {
@@ -15,11 +16,27 @@ const { t } = useI18n({
 
 const route = useRoute()
 
+const fetcher = async (token) =>
+    await fetch(`http://127.0.0.1:8081/logins/${token}`).then(
+        (response) => response.json(),
+    )
+
+const { isPending, isError, isFetching, data, error } = useQuery({
+  queryKey: ['login', route.query.token],
+  queryFn: () => fetcher(route.query.token),
+})
+
 </script>
 
 <template>
   <div class="flex flex-col flex-wrap">
     <p>{{ route.query.token }}</p>
+    <div v-if="isPending" class="update">Loading...</div>
+    <div v-else-if="isError">An error has occurred: {{ error }}</div>
+    <div v-else-if="data">
+      <p>{{ data }}</p>
+      <div v-if="isFetching" class="update">Background Updating...</div>
+    </div>
     <Form
         :title="t('title')"
         :submit-text="t('submit')"
