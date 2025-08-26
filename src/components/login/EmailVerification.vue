5618e2ad36f6fa734e4e5678593169218aeb28bb
Darkarotte <The127@users.noreply.github.com>
2025-08-26 21:10:54 +0200

    Enhance EmailVerification.vue: implement form with Vuelidate validation, i18n support, and resend verification logic placeholder

--- src/components/login/EmailVerification.vue
+++ src/components/login/EmailVerification.vue
@@ -1,5 +1,37 @@
 <script setup>
 
+import Form from "../Form.vue";
+import Heading from "../Heading.vue";
+import {useI18n} from "vue-i18n";
+import Button from "../Button.vue";
+import Input from "../Input.vue";
+import {reactive} from "vue";
+import {required} from "@vuelidate/validators";
+import useVuelidate from "@vuelidate/core";
+
+
+const { t } = useI18n({
+  messages: {
+    en: {
+      title: 'Sign in to {appName}',
+      subTitle: 'Verify email address',
+      explanationText: 'We sent you an email with a verification code. Please enter the code below.',
+      verificationCode: 'Verification code',
+      submit: 'Verify email',
+      resendVerificationEmail: 'Resend verification email',
+    },
+    de: {
+      title: 'In {appName} anmelden',
+      subtitle: 'E-Mail Adresse verifizieren',
+      explanationText: 'Wir haben Ihnen eine E-Mail mit einem Verifizierungscode gesendet. Bitte geben Sie den Code unten ein.',
+      verificationCode: 'Verifizierungscode',
+      submit: 'E-Mail verifizieren',
+      resendVerificationEmail: 'E-Mail erneut senden',
+    },
+  },
+  inheritLocale: true,
+})
+
 const props = defineProps({
   token: {
     type: String,
@@ -13,10 +45,63 @@ const props = defineProps({
 
 const emit = defineEmits(['next'])
 
+const formModel = reactive({
+  verificationToken: '',
+})
+
+const formRules = {
+  verificationToken: { required, },
+}
+
+const v$ = useVuelidate(formRules, formModel)
+
+const onFormSubmit = async () => {
+  alert('todo')
+}
+
+const onResendVerificationMail = async () => {
+  alert('todo')
+}
+
 </script>
 
 <template>
-  EmailVerification
+  <Form
+      :title="t('submit')"
+      :submit-text="t('submit')"
+      @submit="onFormSubmit"
+      :vuelidate="v$"
+  >
+    <template #header>
+      <Heading class="text-center">
+        {{ t('title', { appName: data.virtualServerDisplayName }) }}
+      </Heading>
+      <Heading level="h2" class="text-center">
+        {{ t('subTitle') }}
+      </Heading>
+      <p class="text-center">
+        {{ t('explanationText') }}
+      </p>
+    </template>
+    <Input
+        v-model="v$.verificationToken.$model"
+        :vuelidate="v$.verificationToken"
+        :label="t('verificationCode')"
+        required
+    />
+    <template #footer>
+      <Button
+          variant="special"
+          type="submit"
+          :text="t('submit')"
+      />
+      <Button
+          variant="link"
+          :text="t('resendVerificationEmail')"
+          @click="onResendVerificationMail"
+      />
+    </template>
+  </Form>
 </template>
 
 <style scoped>
