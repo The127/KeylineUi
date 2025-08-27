29744f14f6143c6a1844b262b552c63dc7e0d9b4
karo <karolin.kostial@gmail.com>
2025-08-27 21:30:25 +0200

    Localize `EmailVerified` view with translations and enhance its layout using `Heading` component

--- src/views/EmailVerified.vue
+++ src/views/EmailVerified.vue
@@ -1,9 +1,33 @@
-<script setup lang="ts">
+<script setup>
+
+import {useI18n} from "vue-i18n";
+import Heading from "../components/Heading.vue";
+
+const { t } = useI18n({
+  messages: {
+    en: {
+      title: 'Email verified',
+      text: 'Your email was successfully verified. You may now close this tab.',
+    },
+    de: {
+      title: 'E-Mail verifiziert',
+      text: 'Ihre E-Mail Addresse wurde erfolgreich verifiziert. Sie können dieses Fenster nun schließen.'
+    },
+  },
+  inheritLocale: true,
+})
 
 </script>
 
 <template>
-  Email Verified
+  <div class="flex flex-col flex-wrap gap-5 items-center">
+    <Heading>
+      {{ t('title') }}
+    </Heading>
+    <p class="text-center">
+      {{ t('text') }}
+    </p>
+  </div>
 </template>
 
 <style scoped>
