12fd304f942f5bac6027e0351cf11d9acd996dfb
Darkarotte <The127@users.noreply.github.com>
2025-08-28 21:38:49 +0200

    Enhance SignUp.vue: add Spinner for pending state, improve error handling, update i18n texts, and implement registration confirmation flow

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -4,12 +4,13 @@ import Form from "../components/Form.vue";
 import {useI18n} from "vue-i18n";
 import Heading from "../components/Heading.vue";
 import Button from "../components/Button.vue";
-import {markRaw, reactive} from "vue";
+import {markRaw, reactive, ref} from "vue";
 import {required} from "@vuelidate/validators";
 import useVuelidate from "@vuelidate/core";
 import Input from "../components/Input.vue";
 import {useRoute} from "vue-router";
 import {useMutation, useQuery, useQueryClient} from "@tanstack/vue-query";
+import Spinner from "../components/Spinner.vue";
 
 const route = useRoute()
 const queryClient = useQueryClient()
@@ -44,7 +45,11 @@ const { t } = useI18n({
       password: 'Password',
       dontHaveAnAccount: 'Don\'t have an account?',
       register: 'Sign Up',
-      passwordRequirements: 'Password must be at least 8 characters long.'
+      passwordRequirements: 'Password must be at least 8 characters long.',
+      anErrorHappened: 'An error happened',
+      registrationCompleted: 'Registration completed',
+      youCanNowLogin: 'You can now login with your username and password.',
+      backToLogin: 'Back to login',
     },
     de: {
       title: 'In {appName} registrieren',
@@ -55,7 +60,11 @@ const { t } = useI18n({
       password: 'Passwort',
       dontHaveAnAccount: 'Noch kein Konto?',
       register: 'Registrieren',
-      passwordRequirements: 'Passwort muss mindestens 8 Zeichen lang sein.'
+      passwordRequirements: 'Passwort muss mindestens 8 Zeichen lang sein.',
+      anErrorHappened: 'Ein Fehler ist aufgetreten',
+      registrationCompleted: 'Registrierung abgeschlossen',
+      youCanNowLogin: 'Sie können sich nun mit Ihrem Benutzernamen und Passwort anmelden.',
+      backToLogin: 'Zurück zur Anmeldung',
     },
   },
   inheritLocale: true,
@@ -77,20 +86,35 @@ const formRules = {
 
 const v$ = useVuelidate(formRules, formModel)
 
+const registeringDone = ref(false)
+
 const onFormSubmit = async () => {
-  await register.mutateAsync({
-    username: formModel.username,
-    displayName: formModel.displayName,
-    email: formModel.email,
-    password: formModel.password,
-  })
+  try{
+    await register.mutateAsync({
+      username: formModel.username,
+      displayName: formModel.displayName,
+      email: formModel.email,
+      password: formModel.password,
+    })
+
+    registeringDone.value = true
+  }catch (e) {
+    console.error(e)
+  }
 }
 
 </script>
 
 <template>
+  <div v-if="isPending" class="flex flex-row justify-center items-center">
+    <Spinner/>
+  </div>
+  <div v-else-if="isError">
+    {{ t('anErrorHappened') }}
+    {{ error }}
+  </div>
   <Form
-      v-if="!isPending && !isError && data"
+      v-else-if="!registeringDone"
       :title="t('submit')"
       :submit-text="t('submit')"
       @submit="onFormSubmit"
@@ -136,6 +160,20 @@ const onFormSubmit = async () => {
       <Button variant="special" size="lg" type="submit" :text="t('submit')"/>
     </template>
   </Form>
+  <div v-else class="flex flex-col items-center justify-center">
+    <Heading class="text-center">
+      {{ t('registrationCompleted') }}
+    </Heading>
+    <p class="text-center">
+      {{ t('youCanNowLogin') }}
+    </p>
+    <RouterLink
+        v-if="route.query.token"
+        :to="{name: 'login', query: { token: route.query.token, },}"
+    >
+      {{ t('backToLogin') }}
+    </RouterLink>
+  </div>
 </template>
 
 <style scoped>
