640f0a00bb3f6e794d75cd3fa51ab24d1c6e5faf
Darkarotte <The127@users.noreply.github.com>
2025-08-30 18:48:45 +0200

    Add OIDC authentication to router; implement login callback handling and route guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import {UserManager} from "oidc-client";
 
 const routes = [
     {
@@ -49,8 +50,19 @@ const routes = [
         component: () => import('../views/mgmt/Management.vue'),
         meta: {
             layout: "main-layout",
+            requiresAuth: true,
         },
         children: [
+            {
+                path: 'auth',
+                name: 'mgmt-auth',
+                beforeEnter: async _ => {
+                    return await handleLoginCallback()
+                },
+                meta: {
+                    requiresAuth: false,
+                }
+            },
             {
                 path: '',
                 name: 'mgmt-dashboard',
@@ -60,9 +72,49 @@ const routes = [
     },
 ]
 
+const mgr = new UserManager({
+    authority: "http://127.0.0.1:8081/oidc/keyline",
+    metadataUrl: "http://127.0.0.1:8081/oidc/keyline/.well-known/openid-configuration",
+    client_id: "admin-ui",
+    redirect_uri: "http://localhost:5173/mgmt/keyline/auth",
+    response_type: "code",
+    scope: "oidc profile email",
+})
+
+async function handleLoginCallback() {
+    const user = await mgr.signinRedirectCallback()
+    console.log("redirect callback", user)
+    return user.state.destination;
+}
+
 const router = createRouter({
     history: createWebHistory(),
     routes,
+})
+
+router.beforeEach(async (to, from, next) => {
+    console.log("checking auth")
+    if (to.meta.requiresAuth) {
+        console.log("requires auth")
+        if (await mgr.getUser() === null) {
+            console.log("redirecting to login")
+            const request = await mgr.createSigninRequest(
+                {
+                    state: {
+                        destination: to.fullPath,
+                    }
+                }
+            )
+            window.location.assign("http://" + request.url) // <-- bypasses router
+            next(false)
+        }else {
+            console.log("user is logged in")
+            next()
+        }
+    }else{
+        console.log("no auth")
+        next()
+    }
 })
 
 export default router
