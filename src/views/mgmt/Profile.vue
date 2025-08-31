d991966dfd17830a9acfab73cd69efe799396eda
Darkarotte <The127@users.noreply.github.com>
2025-08-31 18:29:37 +0200

    Add `useQuery` for dynamic user data fetching in `Profile` view

--- src/views/mgmt/Profile.vue
+++ src/views/mgmt/Profile.vue
@@ -2,6 +2,22 @@
 
 import PageHeader from "../../components/PageHeader.vue";
 import PageLayout from "../../components/PageLayout.vue";
+import {useQuery} from "@tanstack/vue-query";
+import {useRoute} from "vue-router";
+import {useUserManager} from "../../composables/userManager.js";
+
+const route = useRoute()
+const userManager = useUserManager(route.params.vsName)
+
+const { isPending, isError, data, error } = useQuery({
+  queryKey: ['profile'],
+  queryFn: async () => {
+    const user = await userManager.getUser()
+    const response = await fetch(`http://127.0.0.1:8081/api/virtual-servers/${route.params.vsName}/users/${user.profile.sub}`)
+    return response.json()
+  }
+})
+
 </script>
 
 <template>
@@ -9,7 +25,11 @@ import PageLayout from "../../components/PageLayout.vue";
     <template #header>
       <PageHeader title="Profile"/>
     </template>
-    Dummy content
+    <div>
+      <template v-if="!isPending && !isError && data">
+      {{ data }}
+      </template>
+    </div>
     <template #footer>
       Footer content
     </template>
