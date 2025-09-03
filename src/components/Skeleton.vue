4f8327c3666fe436a13f5c9f7ae0129fd11d805c
Darkarotte <The127@users.noreply.github.com>
2025-09-03 17:07:46 +0200

    Add `Skeleton` component; supports `text` and `image` loading states

---
+++ src/components/Skeleton.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import {computed} from "vue";
+
+const props = defineProps({
+  dep: {
+    type: Object,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: 'text',
+    validator: v => ['text', 'image'].includes(v),
+  },
+})
+
+const isDependencyLoaded = computed(() => props.dep !== undefined)
+
+</script>
+
+<template>
+  <template v-if="isDependencyLoaded">
+    <slot/>
+  </template>
+  <span
+      v-else
+      class="rounded-full bg-slate-300 animate-pulse"
+  />
+</template>
+
+<style scoped>
+
+</style>
