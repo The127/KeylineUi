104593828203e2359278a0958918f8c734d16d6e
Darkarotte <The127@users.noreply.github.com>
2025-09-03 15:55:01 +0200

    Enhance `buttonVariants`; update styles for consistency and add hover effects

--- src/components/buttonVariants.js
+++ src/components/buttonVariants.js
@@ -2,56 +2,66 @@
 const variants = {
     intent: {
         primary: [
+            "border-emerald-500",
             "bg-emerald-500",
-            "hover:bg-emerald-600",
+            "hover:bg-transparent",
             "text-slate-100",
+            "hover:text-slate-900",
         ],
         secondary: [
-            "border-3",
+            "bg-transparent",
             "border-slate-700",
             "hover:text-slate-100",
             "hover:bg-slate-700",
         ],
         danger: [
-            "border-3",
+            "bg-transparent",
             "border-red-700",
             "hover:text-slate-100",
             "hover:bg-red-700",
         ],
         link: [
+            "bg-transparent",
             "text-emerald-500",
             "hover:text-emerald-600",
             "underline",
             "hover:no-underline",
+            "border-none",
         ],
         special: [
             "bg-gradient-to-r",
             "from-emerald-500", "to-teal-500",
+            "hover:via-emerald-400", "via-teal-500",
             "hover:from-emerald-400", "to-teal-400",
             "text-slate-100",
             "hover:text-slate-50",
             "tracking-wider",
             "uppercase",
+            "border-none",
         ]
     },
     size: {
         inline: [
             "rounded-sm",
+            "border-1",
         ],
         sm: [
             "px-2",
             "py-1",
             "rounded-md",
+            "border-2",
         ],
         md: [
             "px-3",
             "py-2",
             "rounded-md",
+            "border-3",
         ],
         lg: [
             "px-5",
             "py-3",
             "rounded-md",
+            "border-4",
         ],
     },
 }
