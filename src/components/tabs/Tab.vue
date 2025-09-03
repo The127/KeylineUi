7dbf47c93a353f9e827ca42aa2f3958d66e11122
Darkarotte <The127@users.noreply.github.com>
2025-09-03 20:09:10 +0200

    Add `Tabs` and `Tab` components; implement basic tab management system

---
+++ src/components/tabs/Tab.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+import {inject, onMounted, ref} from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+})
+
+const active = ref(false)
+
+const tabManager = inject('tabManager')
+
+const setActive = (value) => {
+  active.value = value
+}
+
+onMounted(() => {
+  tabManager.register({
+    title: props.title,
+    setActive,
+  })
+})
+
+</script>
+
+<template>
+  <template v-if="active">
+    <slot/>
+  </template>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/tabs/Tabs.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+import {ref, provide, onMounted} from "vue";
+
+const tabs = ref([])
+
+const register = (tab) => {
+  for (let t of tabs.value) {
+    if (t.title === tab.title) {
+      return
+    }
+  }
+
+  tabs.value.push(tab)
+
+  if (tabs.value.length === 1) {
+    select(tab)
+    tab.active = true
+  }
+}
+
+provide('tabManager', {
+  register,
+})
+
+const select = (tab) => {
+  for (let t of tabs.value) {
+    if (t.title === tab.title) {
+      t.setActive(true)
+      t.active = true
+    }else{
+      t.setActive(false)
+      t.active = false
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div class="flex flex-row">
+    <div v-for="tab in tabs"
+         @click="select(tab)"
+        :data-active="tab.active ? true : null"
+         class="font-semibold ease-linear px-2 py-1 [[data-active]]:text-emerald-500 hover:text-slate-500 cursor-pointer [[!data-active]]:border-b-transparent [[data-active]]:border-b-emerald-500 border-b-2"
+    >
+      {{ tab.title }}
+    </div>
+  </div>
+  <slot/>
+</template>
+
+<style scoped>
+
+</style>
