91d92cf8945ff318f038ceff25dba44bad14afd9
Darkarotte <The127@users.noreply.github.com>
2025-09-03 15:22:12 +0200

    Enhance `Box` component; add flex and gap utilities for improved layout flexibility

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="bg-slate-50 shadow-md rounded-md p-5">
+  <div class="bg-slate-50 shadow-md rounded-md p-5 flex flex-col gap-5">
     <slot/>
   </div>
 </template>
