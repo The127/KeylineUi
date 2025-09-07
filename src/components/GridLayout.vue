952023e29acad62731beee8473dcf4314d69e69b
Darkarotte <The127@users.noreply.github.com>
2025-09-07 21:51:46 +0200

    Enhance `GridLayout`; update column classes for improved responsiveness

--- src/components/GridLayout.vue
+++ src/components/GridLayout.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+  <div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
     <slot/>
   </div>
 </template>
