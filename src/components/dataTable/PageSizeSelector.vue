9657b67c43c32b56f546762f86dd160ae4771241
Darkarotte <The127@users.noreply.github.com>
2025-09-08 20:01:31 +0200

    Enhance `PageSizeSelector`; replace input with dropdown for predefined page size options

--- src/components/dataTable/PageSizeSelector.vue
+++ src/components/dataTable/PageSizeSelector.vue
@@ -5,7 +5,13 @@ const model = defineModel()
 </script>
 
 <template>
-  <input type="number" v-model="model" />
+  <select v-model="model" class="border rounded px-2 py-1">
+    <option :value="5">5</option>
+    <option :value="10">10</option>
+    <option :value="20">20</option>
+    <option :value="50">50</option>
+    <option :value="100">100</option>
+  </select>
 </template>
 
 <style scoped>
