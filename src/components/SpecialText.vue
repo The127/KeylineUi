cd72594bf1d00acd139f7cf7329de387ac57ced7
Darkarotte <The127@users.noreply.github.com>
2025-09-20 12:44:11 +0200

    Add `SpecialText` component for styled text rendering

---
+++ src/components/SpecialText.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <span class="text-emerald-600 border border-emerald-600 rounded-md text-center text-shadow-2xs">
+    <slot/>
+  </span>
+</template>
+
+<style scoped>
+
+</style>
