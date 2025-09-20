c5f9f6ec7df8131178cffe917c0895645838ee0e
Darkarotte <The127@users.noreply.github.com>
2025-09-20 15:20:21 +0200

    Add `DotMenu` component with customizable size and menu activator slot

---
+++ src/components/DotMenu.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+import Menu from "./menu/Menu.vue";
+import Button from "./Button.vue";
+import { EllipsisVertical } from "lucide-vue-next"
+import Avatar from "./Avatar.vue";
+
+const props = defineProps({
+  size: {
+    type: String,
+    default: 'sm',
+    validator: v => ['sm', 'md', 'lg'].includes(v),
+  },
+})
+
+</script>
+
+<template>
+  <Menu>
+    <template #activator-content="{ attrs }">
+      <Button
+          text="open menu"
+          hide-text
+          variant="secondary"
+          :size="size"
+          v-bind="attrs"
+      >
+        <template #adornment>
+          <EllipsisVertical/>
+        </template>
+      </Button>
+    </template>
+
+    <slot/>
+  </Menu>
+</template>
+
+<style scoped>
+
+</style>
