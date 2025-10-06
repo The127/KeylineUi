4d3c5839a4a59665824493b63c9b64be044ea775
Darkarotte <The127@users.noreply.github.com>
2025-10-06 18:45:03 +0200

    chore: enhance `TemplateDetail` layout with new components for improved structure and readability

--- src/views/mgmt/administration/templates/TemplateDetail.vue
+++ src/views/mgmt/administration/templates/TemplateDetail.vue
@@ -7,6 +7,9 @@ import PageHeader from "../../../../components/PageHeader.vue";
 import ModelMetadata from "../../../../components/ModelMetadata.vue";
 import LoadingSkeleton from "../../../../components/LoadingSkeleton.vue";
 import {getTemplateInfo} from "../templateTypes.js";
+import DataLayout from "../../../../components/dataLayout/DataLayout.vue";
+import DataLayoutItem from "../../../../components/dataLayout/DataLayoutItem.vue";
+import BoxContainer from "../../../../components/BoxContainer.vue";
 
 const route = useRoute()
 
@@ -26,9 +29,17 @@ const {data} = useGetTemplateQuery(
       />
     </template>
 
-    <LoadingSkeleton :dep="data" class="w-32 h-4">
-      {{ data }}
-    </LoadingSkeleton>
+    <BoxContainer>
+      <DataLayout title="Template">
+        <DataLayoutItem title="Text" full-row>
+          <LoadingSkeleton :dep="data" class="w-32 h-4">
+            <p class="border border-slate-200 p-2 rounded-md w-full">
+              {{ data.text }}
+            </p>
+          </LoadingSkeleton>
+        </DataLayoutItem>
+      </DataLayout>
+    </BoxContainer>
 
     <template #footer>
       <ModelMetadata
