2e3e51b9ca2682a7e9c72f577569f8abbbf8b8b2
Darkarotte <The127@users.noreply.github.com>
2025-10-06 18:45:11 +0200

    chore: integrate `@vueup/vue-quill` editor and update `pnpm-lock.yaml` dependencies

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
 import App from './App.vue'
 import router from './router'
 import {i18n} from "./i18n.js";
 import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const app = createApp(App);
+app.component('QuillEditor', QuillEditor)
 app.use(router)
 app.use(i18n)
 app.use(VueQueryPlugin)
