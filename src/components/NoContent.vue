67a48d7e9b224c194967a3fb145eeffcb5c740ea
karo <karolin.kostial@gmail.com>
2025-10-08 19:00:09 +0200

    Add `NoContent` component with conditional rendering and customizable message

---
+++ src/components/NoContent.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import {computed} from "vue";
+
+const props = defineProps({
+  cond: {
+    required: true,
+  },
+  message: {
+    type: String,
+    default: null,
+  }
+})
+
+const messageValue = computed(() => {
+  if (props.message) {
+    return props.message
+  }
+  return "No content"
+})
+
+</script>
+
+<template>
+  <span v-if="cond" class="text-slate-600">
+    {{ messageValue }}
+  </span>
+  <slot v-else/>
+</template>
+
+<style scoped>
+
+</style>
