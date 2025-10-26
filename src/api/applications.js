0384c68842ec67926517d3df1f8950e1159db38f
karo <karolin.kostial@gmail.com>
2025-10-26 21:58:01 +0100

    Add "Applications" tab in `ProjectDetails` view with data table integration and extend API to support project-specific application queries.

--- src/api/applications.js
+++ src/api/applications.js
@@ -2,14 +2,14 @@ import {apiFetch, applyQueryOps} from "./index.js";
 import {useMutation, useQuery, useQueryClient} from "@tanstack/vue-query";
 import {ConfigApiUrl} from "../config.js";
 
-export const useListApplicationQuery = (vsName, queryOps) => useQuery({
+export const useListApplicationQuery = (vsName, projectSlug, queryOps) => useQuery({
     queryKey: ['applications', vsName, queryOps],
-    queryFn: () => listApplicationQueryFn(vsName, queryOps),
+    queryFn: () => listApplicationQueryFn(vsName, projectSlug, queryOps),
 })
 
-export const listApplicationQueryFn = async (vsName, queryOps) => {
+export const listApplicationQueryFn = async (vsName, projectSlug, queryOps) => {
     const url = new URL(
-        ConfigApiUrl() + `/api/virtual-servers/${vsName}/applications`
+        ConfigApiUrl() + `/api/virtual-servers/${vsName}/projects/${projectSlug}/applications`
     )
 
     applyQueryOps(url, queryOps)
@@ -19,33 +19,33 @@ export const listApplicationQueryFn = async (vsName, queryOps) => {
     })
 }
 
-export const useGetApplicationQuery = (vsName, applicationId) => useQuery({
+export const useGetApplicationQuery = (vsName, projectSlug, applicationId) => useQuery({
     queryKey: ['application', vsName, applicationId],
-    queryFn: () => getApplicationQueryFn(vsName, applicationId),
+    queryFn: () => getApplicationQueryFn(vsName, projectSlug, applicationId),
 })
 
-export const getApplicationQueryFn = async (vsName, applicationId) => {
+export const getApplicationQueryFn = async (vsName, projectSlug, applicationId) => {
     return await apiFetch(
-        ConfigApiUrl() + `/api/virtual-servers/${vsName}/applications/${applicationId}`,
+        ConfigApiUrl() + `/api/virtual-servers/${vsName}/projects/${projectSlug}/applications/${applicationId}`,
         {
             vsName: vsName,
         }
     )
 }
 
-export const useCreateApplicationMutation = (vsName) => {
+export const useCreateApplicationMutation = (vsName, projectSlug) => {
     const queryClient = useQueryClient()
     return useMutation({
-        mutationFn: (data) => createApplicationMutation(vsName, data),
+        mutationFn: (data) => createApplicationMutation(vsName, projectSlug, data),
         onSuccess: () => {
             queryClient.invalidateQueries(['applications'])
         }
     })
 }
 
-export const createApplicationMutation = async (vsName, data) => {
+export const createApplicationMutation = async (vsName, projectSlug, data) => {
     return await apiFetch(
-        ConfigApiUrl() + `/api/virtual-servers/${vsName}/applications`,
+        ConfigApiUrl() + `/api/virtual-servers/${vsName}/projects/${projectSlug}/applications`,
     {
             method: 'POST',
             body: JSON.stringify(data),
@@ -54,20 +54,20 @@ export const createApplicationMutation = async (vsName, data) => {
     )
 }
 
-export const usePatchApplicationMutation = (vsName, applicationId) => {
+export const usePatchApplicationMutation = (vsName, projectSlug, applicationId) => {
     const queryClient = useQueryClient()
     return useMutation({
-        mutationFn: (data) => patchApplicationFn(vsName, applicationId, data),
+        mutationFn: (data) => patchApplicationFn(vsName, projectSlug, applicationId, data),
         onSuccess: () => {
             queryClient.invalidateQueries(['applications', applicationId])
         }
     })
 }
 
-export const patchApplicationFn = async (vsName, applicationId, data) => {
+export const patchApplicationFn = async (vsName, projectSlug, applicationId, data) => {
     console.log("patching")
     return await apiFetch(
-        ConfigApiUrl() + `/api/virtual-servers/${vsName}/applications/${applicationId}`,
+        ConfigApiUrl() + `/api/virtual-servers/${vsName}/projects/${projectSlug}/applications/${applicationId}`,
         {
             method: 'PATCH',
             body: JSON.stringify(data),
@@ -76,16 +76,16 @@ export const patchApplicationFn = async (vsName, applicationId, data) => {
     )
 }
 
-export const useDeleteApplicationMutation = (vsName) => {
+export const useDeleteApplicationMutation = (vsName, projectSlug) => {
     const queryClient = useQueryClient()
     return useMutation({
-        mutationFn: (applicationId) => deleteApplicationFn(vsName, applicationId),
+        mutationFn: (applicationId) => deleteApplicationFn(vsName, projectSlug, applicationId),
     })
 }
 
-export const deleteApplicationFn = async (vsName, applicationId) => {
+export const deleteApplicationFn = async (vsName, projectSlug, applicationId) => {
     return await apiFetch(
-        ConfigApiUrl() + `/api/virtual-servers/${vsName}/applications/${applicationId}`,
+        ConfigApiUrl() + `/api/virtual-servers/${vsName}/projects/${projectSlug}/applications/${applicationId}`,
         {
             method: 'DELETE',
             vsName: vsName,
@@ -93,14 +93,14 @@ export const deleteApplicationFn = async (vsName, applicationId) => {
     )
 }
 
-export const useListRolesInApplicationQuery = (vsName, applicationId, queryOps) => useQuery({
+export const useListRolesInApplicationQuery = (vsName, projectSlug, applicationId, queryOps) => useQuery({
     queryKey: ['app-roles', vsName, applicationId, queryOps],
-    queryFn: () => listRolesInApplicationFn(vsName, applicationId, queryOps),
+    queryFn: () => listRolesInApplicationFn(vsName, projectSlug, applicationId, queryOps),
 })
 
-export const listRolesInApplicationFn = async (vsName, applicationId, queryOps) => {
+export const listRolesInApplicationFn = async (vsName, projectSlug, applicationId, queryOps) => {
     const url = new URL(
-        ConfigApiUrl() + `/api/virtual-servers/${vsName}/applications/${applicationId}/roles`
+        ConfigApiUrl() + `/api/virtual-servers/${vsName}/projects/${projectSlug}/applications/${applicationId}/roles`
     )
 
     applyQueryOps(url, queryOps)
--- src/views/mgmt/projects/details/ProjectDetails.vue
+++ src/views/mgmt/projects/details/ProjectDetails.vue
@@ -11,6 +11,10 @@ import DataLayoutItem from "../../../../components/dataLayout/DataLayoutItem.vue
 import LoadingSkeleton from "../../../../components/LoadingSkeleton.vue";
 import TabLayout from "../../../../components/tabs/TabLayout.vue";
 import TabPage from "../../../../components/tabs/TabPage.vue";
+import DataTable from "../../../../components/dataTable/DataTable.vue";
+import {useListApplicationQuery} from "../../../../api/applications.js";
+import DataTableColumn from "../../../../components/dataTable/DataTableColumn.vue";
+import DataTableCell from "../../../../components/dataTable/DataTableCell.vue";
 
 const route = useRoute()
 
@@ -54,7 +58,30 @@ const {data} = useGetProjectQuery(
       </TabPage>
       
       <TabPage title="Applications" name="application">
-        TODO
+        <DataTable
+          enable-search
+          autofocus
+          title="Applications"
+          :queryFn="(pagination) => useListApplicationQuery(route.params.vsName, route.params.projectSlug, pagination)"
+        >
+          <template #columns>
+            <DataTableColumn title="Name" field="name" enable-order initial-order="asc"/>
+            <DataTableColumn title="Display Name" field="display_name" enable-order/>
+            <DataTableColumn title="Namespace" field="system_application" enable-order/>
+          </template>
+
+          <template #row="{ item: app }">
+            <DataTableCell>
+              {{ app.name }}
+            </DataTableCell>
+            <DataTableCell>
+              {{ app.displayName }}
+            </DataTableCell>
+            <DataTableCell>
+              {{ app.systemApplication ? 'System' : 'User' }}
+            </DataTableCell>
+          </template>
+        </DataTable>
       </TabPage>
     </TabLayout>
 
