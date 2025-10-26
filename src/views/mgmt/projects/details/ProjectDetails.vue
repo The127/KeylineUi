48f1bad62f8e103f43286596886a1629ca44b7d4
karo <karolin.kostial@gmail.com>
2025-10-26 19:02:23 +0100

    Add `ProjectDetails` view layout with data fetching, metadata display, and loading states

--- src/views/mgmt/projects/details/ProjectDetails.vue
+++ src/views/mgmt/projects/details/ProjectDetails.vue
@@ -1,9 +1,59 @@
 <script setup>
 
+import PageLayout from "../../../../components/PageLayout.vue";
+import PageHeader from "../../../../components/PageHeader.vue";
+import {useRoute} from "vue-router";
+import {useGetProjectQuery} from "../../../../api/projects.js";
+import ModelMetadata from "../../../../components/ModelMetadata.vue";
+import BoxContainer from "../../../../components/BoxContainer.vue";
+import DataLayout from "../../../../components/dataLayout/DataLayout.vue";
+import DataLayoutItem from "../../../../components/dataLayout/DataLayoutItem.vue";
+import LoadingSkeleton from "../../../../components/LoadingSkeleton.vue";
+
+const route = useRoute()
+
+const {data} = useGetProjectQuery(
+    route.params.vsName,
+    route.params.projectSlug,
+)
+
 </script>
 
 <template>
-asd
+  <PageLayout>
+    <template #header>
+      <PageHeader
+          :title="data?.name"
+          subtitle="Manage project configuration and settings"
+      />
+    </template>
+
+    <BoxContainer>
+      <DataLayout title="Information">
+        <DataLayoutItem title="Name">
+          <LoadingSkeleton :dep="data" class="w-32 h4">
+            {{ data.name }}
+          </LoadingSkeleton>
+        </DataLayoutItem>
+        <DataLayoutItem title="Slug">
+          <LoadingSkeleton :dep="data" class="w-32 h4">
+            {{ data.slug }}
+          </LoadingSkeleton>
+        </DataLayoutItem>
+        <DataLayoutItem title="Description" full-row>
+          <LoadingSkeleton :dep="data" class="w-full h4">
+            {{ data.description }}
+          </LoadingSkeleton>
+        </DataLayoutItem>
+      </DataLayout>
+    </BoxContainer>
+
+    <template #footer>
+      <ModelMetadata
+          :model="data"
+      />
+    </template>
+  </PageLayout>
 </template>
 
 <style scoped>
