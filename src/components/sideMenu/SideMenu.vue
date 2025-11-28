fff269b90832bc5eb1f7d87bfbb1ba8bacb28eab
karo <karolin.kostial@gmail.com>
2025-11-28 20:39:52 +0100

    Add `link` class to `RouterLink` in `SideMenu` for consistent styling

--- src/components/sideMenu/SideMenu.vue
+++ src/components/sideMenu/SideMenu.vue
@@ -8,7 +8,7 @@ import KeylineIcon from "../icons/KeylineIcon.vue";
 
   <div class="flex flex-col w-80 bg-slate-100 border-r border-r-slate-200">
     <div class="h-20 mx-5 border-b border-b-slate-200 flex flex-row items-center">
-      <RouterLink :to="{name: 'mgmt-dashboard'}" class="flex flex-row gap-2 items-center">
+      <RouterLink :to="{name: 'mgmt-dashboard'}" class="flex flex-row gap-2 items-center link">
         <KeylineIcon class="w-12 h-12 "/>
         <span class="text-xl">Keyline</span>
       </RouterLink>
